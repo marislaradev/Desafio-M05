fcfc676a69d10c15d2d1cb3c4d50d77c66968b4d
paularml <paula.rml@hotmail.com>
2023-10-13 22:54:54 -0300

    refactor: nome, email senha characters limitation

--- dump.sql
+++ dump.sql
@@ -24,3 +24,49 @@ VALUES
     ('Moda'),
     ('Bebê'),
     ('Games');
+
+-- nome, email senha characters limitation
+
+ALTER TABLE usuarios
+ADD COLUMN novo_nome VARCHAR
+(255);
+
+
+
+ALTER TABLE usuarios
+ADD COLUMN novo_email VARCHAR
+(320);
+
+ALTER TABLE usuarios
+ADD COLUMN novo_senha VARCHAR
+(128);
+
+UPDATE usuarios
+SET novo_nome = nome;
+
+UPDATE usuarios
+SET novo_email = email;
+
+UPDATE usuarios
+SET novo_senha = senha;
+
+
+ALTER TABLE usuarios
+DROP COLUMN nome;
+
+ALTER TABLE usuarios
+DROP COLUMN email;
+
+ALTER TABLE usuarios
+DROP COLUMN senha;
+
+ALTER TABLE usuarios
+RENAME COLUMN novo_nome TO nome;
+
+
+ALTER TABLE usuarios
+RENAME COLUMN novo_email TO email;
+
+
+ALTER TABLE usuarios
+RENAME COLUMN novo_senha TO senha;
