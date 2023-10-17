7b07e0e23ba2f434b39f96563427d41bae924d42
alinesantana13 <97478571+alinesantana13@users.noreply.github.com>
2023-10-16 22:35:21 -0300

    refactor: message update

--- src/controllers/updateUser.js
+++ src/controllers/updateUser.js
@@ -6,13 +6,13 @@ const updateUser = async (req, res) => {
   const userId = req.user.id;
 
   try {
-  
+
     const whiteSpace = (str) => {
       return str.trim().length === 0;
     };
 
     if (whiteSpace(nome) || whiteSpace(email) || whiteSpace(senha)) {
-      return res.status(400).json({ mensagem: 'Não foram preenchidos todos os campos' });
+      return res.status(400).json({ mensagem: 'Um ou mais campos foram preenchidos com espaços em branco' });
     }
 
     const verifyEmailUnique = await knex('usuarios').where({ email }).andWhere('id', '!=', userId);
