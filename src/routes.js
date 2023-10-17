b5985d38b3e17d2c31e0eefeb6ac7df2d2a293bc
paularml <paula.rml@hotmail.com>
2023-10-17 20:36:23 -0300

    feat: delete route

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ const verifyLogin = require('./middleware/authentication');
 const validateRequestBody = require('./middleware/validateRequestBody');
 const schemaLogin = require('./schemas/schemaLogin');
 const schemaUser = require('./schemas/schemaUser');
+const deleteProduct = require('./controllers/deleteProduct');
 
 const routes = express();
 
@@ -20,5 +21,6 @@ routes.use(verifyLogin);
 
 routes.get('/usuario', detailUser);
 routes.put('/usuario', validateRequestBody(schemaUser), updateUser);
+routes.delete('/produto/:id', deleteProduct);
 
 module.exports = routes;
