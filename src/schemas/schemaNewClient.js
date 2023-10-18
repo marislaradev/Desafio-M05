2688bd732af22a5a50eeefcdb3af2af35e0866e3
paularml <paula.rml@hotmail.com>
2023-10-18 01:30:07 -0300

    feat: new client schema 2nd part/working

--- src/schemas/schemaNewClient.js
+++ src/schemas/schemaNewClient.js
@@ -3,13 +3,21 @@ const joi = require('joi');
 const schemaNewClient = joi.object({
     nome: joi.string().required().messages({
         'any.required': 'O nome é obrigatório',
-        'string.empty': 'O nome é obrigatório'
+        'string.empty': 'O nome é obrigatório',
+
     }),
     email: joi.string().email().required().messages({
         'string.email': 'O email precisa ter um formato válido',
         'any.required': 'O email é obrigatório',
         'string.empty': 'O email é obrigatório'
-    })
+    }),
+    cpf: joi.string(),
+    cep: joi.string(),
+    rua: joi.string(),
+    numero: joi.string(),
+    bairo: joi.string(),
+    cidade: joi.string(),
+    estado: joi.string(),
 });
 
 module.exports = schemaNewClient;
