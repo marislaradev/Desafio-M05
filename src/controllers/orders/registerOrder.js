f660623e08d9ff3aa336f7bf00bc34056357c6c0
paularml <paula.rml@hotmail.com>
2023-10-24 20:25:12 -0300

    refactor: registerOrder validations

--- src/controllers/orders/registerOrder.js
+++ src/controllers/orders/registerOrder.js
@@ -4,39 +4,54 @@ const registerOrder = async (req, res) => {
     const { cliente_id, observacao, pedido_produtos } = req.body;
 
     try {
-
         const verifyClienteId = await knex('clientes').where({ id: cliente_id });
-        console.log(verifyClienteId)
+        // console.log(verifyClienteId)
         if (!verifyClienteId.length) {
             return res.status(404).json({ mensagem: 'O servidor não pode encontrar o cliente.' }); // melhorar msg de erro?
         }
-
+        // array vazio
         if (!pedido_produtos) {
-            return res.status(404).json({ mensagem: 'Sem produtos.' }); // melhorar msg de erro?
+            return res.status(400).json({ mensagem: 'Sem produtos.' }); // melhorar msg de erro?
         }
 
-        const productsIdList = pedido_produtos.map(item => item.produto_id.toString());
-        const productsIdArray = productsIdList.map(Number);
+        for (const produto of pedido_produtos) {
+            const verifyProduct = await knex('produtos').where({ id: produto.produto_id }).first();
+            console.log(verifyProduct)
+            if (!verifyProduct) {
+                return res.status(404).json({ mensagem: `id errado ${produto.produto_id}` })
+            }
 
-        const productIdDB = await knex('produtos').pluck('id')
-        console.log(productIdDB)
+            if (verifyProduct.quantidade_estoque < produto.quantidade_produto) {
+                return res.status(404).json({ mensagem: `quantidade do produto com id ${produto.produto_id} excede o estoque` })
+            }
 
-        console.log(productsIdArray)
 
-        const itemEhValido = productsIdArray.every(item => productIdDB.includes(item))
 
-        if (!itemEhValido) {
-            return res.status(404).json({ mensagem: 'Id do produto inválido.' });
         }
 
-        const productQuantity = pedido_produtos.map(item => item.quantidade_produto.toString());
 
-        const productsQuantityArray = productQuantity.map(Number);
+        // const productsIdList = pedido_produtos.map(item => item.produto_id.toString());
+        // const productsIdArray = productsIdList.map(Number); //ver separação
 
-        const productQuantityDB = await knex('produtos').pluck('quantidade_estoque')
-        console.log(productsQuantityArray)
+        // const productIdDB = await knex('produtos').pluck('id')
+        // console.log(productIdDB)
 
-        console.log(productQuantityDB)
+        // console.log(productsIdArray)
+
+        // const itemEhValido = productsIdArray.every(item => productIdDB.includes(item))
+
+        // if (!itemEhValido) {
+        //     return res.status(404).json({ mensagem: 'Id do produto inválido.' });
+        // }
+
+        // const productQuantity = pedido_produtos.map(item => item.quantidade_produto.toString());
+
+        // const productsQuantityArray = productQuantity.map(Number);
+
+        // const productQuantityDB = await knex('produtos').pluck('quantidade_estoque')
+        // console.log(productsQuantityArray)
+
+        // console.log(productQuantityDB)
 
 
 
