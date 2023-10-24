449ada04bf0f632ca335c990449383fe3568f15f
paularml <paula.rml@hotmail.com>
2023-10-24 20:38:06 -0300

    refactor: cliente_id validation

--- src/controllers/orders/registerOrder.js
+++ src/controllers/orders/registerOrder.js
@@ -2,7 +2,9 @@ const knex = require('../../database/connection');
 
 const registerOrder = async (req, res) => {
     const { cliente_id, observacao, pedido_produtos } = req.body;
-
+    if (!cliente_id) {
+        return res.status(404).json({ mensagem: 'O servidor não pode encontrar o cliente.' }); // melhorar msg de erro?
+    }
     try {
         const verifyClienteId = await knex('clientes').where({ id: cliente_id });
         // console.log(verifyClienteId)
@@ -10,7 +12,7 @@ const registerOrder = async (req, res) => {
             return res.status(404).json({ mensagem: 'O servidor não pode encontrar o cliente.' }); // melhorar msg de erro?
         }
         // array vazio
-        if (!pedido_produtos) {
+        if (!pedido_produtos || pedido_produtos.length === 0) {
             return res.status(400).json({ mensagem: 'Sem produtos.' }); // melhorar msg de erro?
         }
 
@@ -24,39 +26,8 @@ const registerOrder = async (req, res) => {
             if (verifyProduct.quantidade_estoque < produto.quantidade_produto) {
                 return res.status(404).json({ mensagem: `quantidade do produto com id ${produto.produto_id} excede o estoque` })
             }
-
-
-
         }
-
-
-        // const productsIdList = pedido_produtos.map(item => item.produto_id.toString());
-        // const productsIdArray = productsIdList.map(Number); //ver separação
-
-        // const productIdDB = await knex('produtos').pluck('id')
-        // console.log(productIdDB)
-
-        // console.log(productsIdArray)
-
-        // const itemEhValido = productsIdArray.every(item => productIdDB.includes(item))
-
-        // if (!itemEhValido) {
-        //     return res.status(404).json({ mensagem: 'Id do produto inválido.' });
-        // }
-
-        // const productQuantity = pedido_produtos.map(item => item.quantidade_produto.toString());
-
-        // const productsQuantityArray = productQuantity.map(Number);
-
-        // const productQuantityDB = await knex('produtos').pluck('quantidade_estoque')
-        // console.log(productsQuantityArray)
-
-        // console.log(productQuantityDB)
-
-
-
         return res.status(200).json()
-
     }
     catch (error) {
         console.log(error)
