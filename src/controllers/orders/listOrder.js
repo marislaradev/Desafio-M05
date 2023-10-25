fdbd18a60cca4be1e13f278cf6167d9a50c71671
Bruna Fraga <brunafraga12@gmail.com>
2023-10-25 14:49:05 -0300

    Update listOrder

--- src/controllers/orders/listOrder.js
+++ src/controllers/orders/listOrder.js
@@ -1,46 +1,70 @@
 const knex = require('../../database/connection');
 
-    const listOrder = async (req, res) => {
-        const { cliente_id } = req.query;
-    
-        try {
-            const query = knex('pedidos as p')
-                .select(
-                    'p.id as pedido_id',
-                    'p.cliente_id',
-                    'p.observacao',
-                    'p.valor_total',
-                    'pp.id as pedido_produto_id',
-                    'pp.produto_id',
-                    'pp.quantidade_produto',
-                    'pp.valor_produto',
-                    'prod.descricao as produto_descricao'
-                )
-                .join('pedido_produtos as pp', 'p.id', 'pp.pedido_id')
-                .join('produtos as prod', 'pp.produto_id', 'prod.id');
-    
-            if (cliente_id) {
-                query.where('p.cliente_id', cliente_id);
+const listOrder = async (req, res) => {
+    const { cliente_id } = req.query;
+
+    try {
+
+        const clientExists = await knex('clientes').where({ id: cliente_id });
+
+        if (cliente_id && !clientExists.length) {
+            return res.status(404).json({ mensagem: 'Cliente não encontrado' });
+        }
+
+        const query = knex('pedidos as p')
+            .select(
+                'p.id as pedido_id',
+                'p.cliente_id',
+                'p.observacao',
+                'p.valor_total',
+                'pp.id as pedido_produto_id',
+                'pp.produto_id',
+                'pp.quantidade_produto',
+                'pp.valor_produto',
+                'prod.descricao as produto_descricao'
+            )
+            .join('pedido_produtos as pp', 'p.id', 'pp.pedido_id')
+            .join('produtos as prod', 'pp.produto_id', 'prod.id');
+
+        if (cliente_id) {
+            query.where('p.cliente_id', cliente_id);
+        }
+
+        const orders = await query;
+
+        const formattedOrders = [];
+        let currentOrder = null;
+
+        for (const order of orders) {
+            if (!currentOrder || currentOrder.pedido_id !== order.pedido_id) {
+
+                currentOrder = {
+                    pedido: {
+                        id: order.pedido_id,
+                        valor_total: 0, 
+                        observacao: order.observacao,
+                        cliente_id: order.cliente_id,
+                    },
+                    pedido_produtos: [],
+                };
+                formattedOrders.push(currentOrder);
             }
-    
-            const orders = await query;
-    
-            const ordersWithTotalValue = orders.map(order => ({
-                pedido_id: order.pedido_id,
-                cliente_id: order.cliente_id,
-                observacao: order.observacao,
-                valor_total: order.quantidade_produto * order.valor_produto,
-                pedido_produto_id: order.pedido_produto_id,
-                produto_id: order.produto_id,
+
+            currentOrder.pedido_produtos.push({
+                id: order.pedido_produto_id,
                 quantidade_produto: order.quantidade_produto,
                 valor_produto: order.valor_produto,
-                produto_descricao: order.produto_descricao,
-            }));
-    
-            return res.status(200).json(ordersWithTotalValue);
-        } catch (error) {
-            return res.status(500).json({ mensagem: "Erro inesperado do servidor." });
+                pedido_id: order.pedido_id,
+                produto_id: order.produto_id,
+            });
+
+            currentOrder.pedido.valor_total += order.quantidade_produto * order.valor_produto;
         }
-    };
-    
-    module.exports = listOrder;
+
+        return res.status(200).json(formattedOrders);
+    } catch (error) {
+        return res.status(500).json({ mensagem: "Erro inesperado do servidor." });
+    }
+};
+
+module.exports = listOrder;
