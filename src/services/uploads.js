61bec2b8e3cadb914f5251d0d368af08f2cfe795
Mariana da Silva Lara <marislaradev@outlook.com>
2023-10-25 19:51:52 -0300

    feat: product image upload

---
+++ src/services/uploads.js
@@ -0,0 +1,29 @@
+const aws = require('aws-sdk');
+
+const endpoint = new aws.Endpoint(process.env.ENDPOINT_BACKBLAZE);
+
+const s3 = new aws.S3({
+    endpoint,
+    credentials: {
+        accessKeyId: process.env.KEY_ID,
+        secretAccessKey: process.env.APP_KEY
+    }
+})
+
+const uploadImage = async (path, buffer, mimetype) => {
+    const image = await s3.upload({
+        Bucket: process.env.BUCKET_NAME,
+        Key: path,
+        Body: buffer,
+        ContentType: mimetype
+    }).promise()
+
+    return {
+        path: image.Key,
+        URL: `https://${process.env.BUCKET_NAME}.${process.env.ENDPOINT_BACKBLAZE}/${image.Key}`
+    }
+}
+
+module.exports = {
+    uploadImage
+}
