e65a9214d68b2ab5bc51c261d36be1d9e47dddd9
Mariana da Silva Lara <marislaradev@outlook.com>
2023-10-26 11:03:38 -0300

    fix(registerProduct): response and validation updates

--- src/controllers/products/registerProduct.js
+++ src/controllers/products/registerProduct.js
@@ -3,17 +3,21 @@ const { uploadImage } = require('../../services/uploads');
 
 const registerProduct = async (req, res) => {
     const { descricao, quantidade_estoque, valor, categoria_id  } = req.body
-    const { originalname, mimetype, buffer } = req.file;
+    
+    let imageUrl = null;
 
     try {
-        console.log('Iniciando o cadastro de produto...');
         const verifyCategory = await knex('categorias')
         .where({ id: categoria_id })
         .first();
 
         if (!verifyCategory) {
             console.log('Categoria inválida.');
-            return res.status(400).json({ mensagem: 'A categoria que foi informada não existe' })
+            return res.status(400).json({ mensagem: 'A categoria informada não existe' });
+        }
+
+        if (quantidade_estoque <= 0 || valor <= 0) {
+            return res.status(400).json({ mensagem: 'A quantidade de estoque e o valor não podem ser zerados ou negativos'});
         }
 
         let product = await knex('produtos')
@@ -21,33 +25,42 @@ const registerProduct = async (req, res) => {
             descricao,
             quantidade_estoque,
             valor,
-            categoria_id
+            categoria_id,
+            produto_imagem: imageUrl,
         }).returning('*');
 
-        if (!product) {
-            console.log('Erro ao inserir o produto no banco de dados.');
+        if (!product || !product[0]) {
             return res.status(201).json({ mensagem: 'O produto não foi cadastrado'});
         }
 
         const id = product[0].id;
 
-        const image = await uploadImage(
-            `produto/${id}/${originalname}`,
-            buffer,
-            mimetype
-        )
-        
-        product = await knex('produtos').update({
-            produto_imagem: image.path
-        }).where({ id }).returning('*');
+        if (req.file) {
+            const { originalname, mimetype, buffer } = req.file;
+            const image = await uploadImage(
+                `produto/${id}/${originalname}`,
+                buffer,
+                mimetype
+            );
+            imageUrl = image.URL;
+        }
+    
+        product = await knex('produtos')
+        .update({
+            produto_imagem: imageUrl
+        }).where({ id })
+        .returning('*');
 
-        console.log('Produto cadastrado com sucesso.');
+        const response = {
+            descricao: product[0].descricao,
+            quantidade_estoque: product[0].quantidade_estoque,
+            valor: product[0].valor,
+            categoria_id: product[0].categoria_id,
+            produto_imagem: product[0].produto_imagem,
+        };
 
-        product[0].urlImage = image.URL;
-
-        return res.status(200).json(product[0]);
+        return res.status(200).json(response);
     } catch (error) {
-        console.log('Erro inesperado do servidor:', error);
         return res.status(500).json({ mensagem: "Erro inesperado do servidor." });
     }
 };
